b13d2aa5f4abfc669d4cc4ade7f40c0602f71a70
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2023-07-15 15:38:18 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    tempertaure = 23
+    return {"station": station,
+            "date": date,
+            "tempertaure": tempertaure}
+    #return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
